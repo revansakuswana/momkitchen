9d62aff33a82a61527a13aa7e73491829f33a754
Revansa Helsa Kuswana <revansakuswana@gmail.com>
2025-03-03 19:21:42 +0700

    fix: resolve route path

--- frontend/src/routes/Routes.jsx
+++ frontend/src/routes/Routes.jsx
@@ -1,5 +1,5 @@
 import { Routes, Route } from "react-router-dom";
-import Home from "@pages/home.jsx";
+import Home from "@pages/Home.jsx";
 import Signin from "@pages/Signin.jsx";
 import Signup from "@pages/Signup.jsx";
 import Menu from "@pages/Menu.jsx";
